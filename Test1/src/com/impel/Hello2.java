3a314be5b81634b5a6e34a0671f84bc7c0d2413d
milinkorath <milinkorath@gmail.com>
2015-05-08 17:08:59 +0530

    =fixed compile time errir

--- Test1/src/com/impel/Hello2.java
+++ Test1/src/com/impel/Hello2.java
@@ -5,13 +5,10 @@ public class Hello2 {
 	public static void main(String[] args) {
 		
 		System.out.println("just adding one more file");
-<<<<<<< HEAD
 		System.out.println("This is master branch commit");
-=======
 		System.out.println("just adding one more file");
 		System.out.println("just adding one more file");
 		System.out.println("just adding one more file");
 		System.out.println("just adding one more file");
->>>>>>> testing
 	}
 }
