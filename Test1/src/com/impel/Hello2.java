5a89060a38c4ad9e543b3c2c16ea812fd7f6b37e
milinkorath <milinkorath@gmail.com>
2015-05-08 16:55:25 +0530

    master branch commit

--- Test1/src/com/impel/Hello2.java
+++ Test1/src/com/impel/Hello2.java
@@ -5,5 +5,6 @@ public class Hello2 {
 	public static void main(String[] args) {
 		
 		System.out.println("just adding one more file");
+		System.out.println("This is master branch commit ");
 	}
 }
