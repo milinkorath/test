24d98406c4a34cd860db05333a6e36b8bfecb1f5
milinkorath <milinkorath@gmail.com>
2015-05-08 16:16:22 +0530

    again i modified hellio

--- Test1/src/com/impel/Hello.java
+++ Test1/src/com/impel/Hello.java
@@ -5,5 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Again hello");
+		System.out.println("Again hello");
+		System.out.println("Again hello");
 	}
 }
