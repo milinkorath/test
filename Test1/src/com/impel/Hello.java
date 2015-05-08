352dae87a0e28c35fda52b0807a25e438680013b
milinkorath <milinkorath@gmail.com>
2015-05-08 16:28:13 +0530

    My 4th commit

--- Test1/src/com/impel/Hello.java
+++ Test1/src/com/impel/Hello.java
@@ -5,7 +5,5 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Again hello");
-		System.out.println("Again hello");
-		System.out.println("Again hello");
 	}
 }
