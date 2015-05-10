bf8b77b1e8919c386f680dd3b18f91747fbcca5c
milin <milinkp@gmail.com>
2015-05-10 09:39:22 +0530

    Just checking again

--- Test1/src/com/impel/Hello3.java
+++ Test1/src/com/impel/Hello3.java
@@ -4,5 +4,7 @@ public class Hello3 {
 
 	public static void main(String[] args) {
 		System.out.println("Checking again");
+		System.out.println("Hi Again");
+		System.out.println("Hi Again");
 	}
 }
