b9bdbc9226ca0981657ac0ae8764b5c1efe13c81
milin <milinkp@gmail.com>
2015-05-10 09:37:32 +0530

    commiting new changes

--- Test1/src/com/impel/HotFix.java
+++ Test1/src/com/impel/HotFix.java
@@ -4,7 +4,11 @@ public class HotFix {
 	
 	public static void main(String[] args) {
 		
+		System.out.println("Just checking");
 		System.out.println("Just adding a fix");
+		System.out.println("Just adding a fix");
+		
 	}
+	
 
 }
--- Test1/src/com/impel/HotFix2.java
+++ Test1/src/com/impel/HotFix2.java
@@ -2,4 +2,5 @@ package com.impel;
 
 public class HotFix2 {
 /// Just adding hot fix2
+	//checking
 }
